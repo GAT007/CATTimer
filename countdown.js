f8b6f7a1d0876c676e99345214611e54ecb1b036
Amith Tallanki <GAT007@users.noreply.github.com>
2017-10-14 11:18:28 +0530

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,5 +1,5 @@
 // set the date we're counting down to
-var target_date = new Date("Nov 26, 2017").getTime();
+var target_date = new Date("Nov 24, 2017").getTime();
  
 // variables for time units
 var days, hours, minutes, seconds;
